997029cb829b85be74bd5fae1979a72184db6034
s0zzang <dlth8888@gmail.com>
2025-06-28 18:41:47 +0900

    Done: 초기 데이터(model) 삭제

--- utils/storage.js
+++ utils/storage.js
@@ -1,11 +1,9 @@
-import { model } from "../model/model.js";
-
 export const setStorage = (data) => {
   if (!data) return;
   window.localStorage.setItem("todoList", JSON.stringify(data));
 };
 
 export const getStorage = () => {
-  if (!window.localStorage.getItem("todoList")) setStorage(model);
+  if (!window.localStorage.getItem("todoList")) setStorage([]);
   return JSON.parse(window.localStorage.getItem("todoList"));
 };
